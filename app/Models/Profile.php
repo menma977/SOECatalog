3df21ef55cac93e00e66dd5e4aa041f006b78ca0
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-18 14:21:04 +0700

    Update Profile.php

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Class Profile
+ * @package App\Models
+ * @property integer id
+ * @property string first_name
+ * @property string last_name
+ * @property string address
+ */
 class Profile extends Model
 {
-    use HasFactory;
+  use HasFactory, SoftDeletes;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'first_name',
+    'last_name',
+    'address',
+  ];
+
+  public function user() {
+    $this->hasOne(User::class, 'profile_id', 'id');
+  }
 }
