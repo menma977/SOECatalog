23e8297730686b82ceedfd34a9ca8286563a420f
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-11-18 15:38:45 +0700

    seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -16,9 +17,32 @@ class UserSeeder extends Seeder
   public function run()
   {
     //default user
+    $profile = new Profile();
+    $profile->first_name = "Putra";
+    $profile->last_name = "Dewa";
+    $profile->address = "JL. Terate No 75 Madiun Jawa Timur Indoneisa";
+    $profile->save();
+
     $user = new User();
-    $user->name = "admin";
-    $user->username = "admin";
+    $user->role = 1;
+    $user->profile_id = $profile->id;
+    $user->email = "com.owl.minerva@gmail.com";
+    $user->username = "menma977";
+    $user->password = Hash::make("admin");
+    $user->phone = "081211610807";
+    $user->save();
+
+    $profile = new Profile();
+    $profile->first_name = "Ika";
+    $profile->last_name = "Nurfallah";
+    $profile->address = "JL. Terate No 75 Madiun Jawa Timur Indoneisa";
+    $profile->save();
+
+    $user = new User();
+    $user->role = 2;
+    $user->profile_id = $profile->id;
+    $user->email = "ikanurfallah.use@gmail.com";
+    $user->username = "ika";
     $user->password = Hash::make("admin");
     $user->phone = "081211610807";
     $user->save();
