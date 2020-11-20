e68fd67129976070578d0751328d8e7d975d9ad6
SusuTawar <realsusutawar@gmail.com>
2020-11-20 16:06:23 +0700

    fixing category table wont show

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,73 +1,74 @@
 @extends('layouts.app')
 
 @section('title')
-  <div class="row mb-2">
-    <div class="col-sm-6">
-      <h1>Category</h1>
-    </div>
-    <div class="col-sm-6">
-      <ol class="breadcrumb float-sm-right">
-        <li class="breadcrumb-item"><a href="{{ route('dashboard.index')}}">Dashboard</a></li>
-        <li class="breadcrumb-item active">Category</li>
-      </ol>
-    </div>
+<div class="row mb-2">
+  <div class="col-sm-6">
+    <h1>Category</h1>
   </div>
+  <div class="col-sm-6">
+    <ol class="breadcrumb float-sm-right">
+      <li class="breadcrumb-item"><a href="{{ route('dashboard.index')}}">Dashboard</a></li>
+      <li class="breadcrumb-item active">Category</li>
+    </ol>
+  </div>
+</div>
 @endsection
 
 @section('content')
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <form id="_up" action="{{ route('category.store') }}" method="post">
-          @csrf
-          <div class="card bg-pink collapsed-card elevation-3">
-            <div class="card-header">
-              <h3 class="card-title">Create Category</h3>
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
-                </button>
-              </div>
-            </div>
-            <div class="card-body">
-              <div class="form-group">
-                <label for="_name">Name</label>
-                <input type="text" class="form-control" id="_name" name="name" placeholder="Enter name">
-              </div>
-              <div class="form-group">
-                <label for="_description">Description</label>
-                <textarea class="form-control" id="_description" name="description" rows="4" placeholder="Enter description ..."></textarea>
-              </div>
-            </div>
-            <div class="card-footer">
-              <div class="row">
-                <div class="col-md-6 mb-1">
-                  <button id="_cancel" type="button" class="btn btn-block bg-pink btn-sm elevation-2">Cancel</button>
-                </div>
-                <div class="col-md-6 mb-1">
-                  <button type="submit" class="btn btn-block bg-pink btn-sm elevation-2">Submit</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div class="col-12">
-        <div class="card elevation-2">
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <form id="_up" action="{{ route('category.store') }}" method="post">
+        @csrf
+        <div class="card bg-pink collapsed-card elevation-3">
           <div class="card-header">
-            <h3 class="card-title">List Category</h3>
+            <h3 class="card-title">Create Category</h3>
             <div class="card-tools">
-              <div class="input-group input-group-sm" style="width: 250px;">
-                <input id="_filter" type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                <div class="input-group-append">
-                  <label for="_filter" class="btn btn-default"><i class="fas fa-search"></i></label>
-                </div>
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
+              </button>
+            </div>
+          </div>
+          <div class="card-body">
+            <div class="form-group">
+              <label for="_name">Name</label>
+              <input type="text" class="form-control" id="_name" name="name" placeholder="Enter name">
+            </div>
+            <div class="form-group">
+              <label for="_description">Description</label>
+              <textarea class="form-control" id="_description" name="description" rows="4"
+                placeholder="Enter description ..."></textarea>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col-md-6 mb-1">
+                <button id="_cancel" type="button" class="btn btn-block bg-pink btn-sm elevation-2">Cancel</button>
+              </div>
+              <div class="col-md-6 mb-1">
+                <button type="submit" class="btn btn-block bg-pink btn-sm elevation-2">Submit</button>
               </div>
             </div>
           </div>
-          <!-- /.card-header -->
-          <div class="card-body table-responsive p-0" style="height: 300px;">
-            <table id="contentTable" class="table table-head-fixed text-nowrap table-striped" style="width: 100%">
-              <thead class="text-center">
+        </div>
+      </form>
+    </div>
+    <div class="col-12">
+      <div class="card elevation-2">
+        <div class="card-header">
+          <h3 class="card-title">List Category</h3>
+          <div class="card-tools">
+            <div class="input-group input-group-sm" style="width: 250px;">
+              <input id="_filter" type="text" name="table_search" class="form-control float-right" placeholder="Search">
+              <div class="input-group-append">
+                <label for="_filter" class="btn btn-default"><i class="fas fa-search"></i></label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-0" style="height: 300px;">
+          <table id="contentTable" class="table table-head-fixed text-nowrap table-striped" style="width: 100%">
+            <thead class="text-center">
               <tr>
                 <th style="width: 5%">ID</th>
                 <th style="width: 50%">Name</th>
@@ -75,8 +76,8 @@
                 <th style="width: 50%">Date</th>
                 <th style="width: 50%">Action</th>
               </tr>
-              </thead>
-              <tbody>
+            </thead>
+            <tbody>
               <template id="template_row">
                 <tr id="logData">
                   <td class="id"></td>
@@ -93,18 +94,18 @@
                   </td>
                 </tr>
               </template>
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
   </div>
+</div>
 @endsection
 
 @section('addJs')
-  <script>
-    const table = document.querySelector("#contentTable")
+<script>
+  const table = document.querySelector("#contentTable")
     const row = document.querySelector('#template_row').content.querySelector("tr");
 
     $(function () {
@@ -150,6 +151,7 @@
           newRow.querySelector(".edit").id = newRow.querySelector(".edit").id.replace("#id#", data.id)
           newRow.querySelector(".edit").name = newRow.querySelector(".edit").name.replace("#name#", data.name)
           newRow.querySelector(".delete").href = newRow.querySelector(".delete").href.replace("#id#", data.id)
+          new_tbody.appendChild(newRow);
           if (old_tbody) {
             old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
           } else {
@@ -160,5 +162,5 @@
         console.log(error);
       });
     }
-  </script>
+</script>
 @endsection
